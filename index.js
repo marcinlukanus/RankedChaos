15cecf43f0b9957100f66b266ba963906fa0d057
unknown <mlukanus@gmail.com>
2020-10-24 22:47:59 -0500

    Adds dotenv package to store bot's token and updates .gitignore

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { prefix } = require('./config.json');
+const dotenv = require('dotenv');
+dotenv.config();
 
 client.once('ready', () => {
 	console.log('Ready!');
