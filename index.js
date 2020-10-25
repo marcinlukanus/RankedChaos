4fdbd1589b34b3df00a9a2878f8bdd65988a02ac
unknown <mlukanus@gmail.com>
2020-10-25 11:35:39 -0500

    Adds mongoose and setup file for it (credit YT/NerdCaveDevelopment)

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ const { prefix } = require('./config.json');
 const dotenv = require('dotenv');
 dotenv.config();
 
+client.mongoose = require('./utils/mongoose');
+
 client.once('ready', () => {
 	console.log('Ready!');
 });
 
+client.mongoose.init();
 client.login(process.env.RANKEDCHAOS_TOKEN);
