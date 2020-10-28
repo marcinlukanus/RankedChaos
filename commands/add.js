62c5761d223977b80d71cbf953f4b56ce8838d17
unknown <mlukanus@gmail.com>
2020-10-27 21:35:11 -0500

    Adds a few extra keys to the Players schema

--- commands/add.js
+++ commands/add.js
@@ -3,14 +3,26 @@ const Players = require('../models/players');
 module.exports = {
     name: 'add',
     description: 'Adds player to Players model',
-    execute(message) {
-		const newPlayer = new Players({
-            discordId: message.author,
-            displayName: message.member.displayName,
-        });
+    execute(message, args) {
+        const server = args[0] ? args.toUpperCase() : 'US-EAST';
 
-        Players.create(newPlayer);
+        if (server === 'US-EAST' ||
+            server === 'US-WEST' ||
+            server === 'EU') {
+            const newPlayer = new Players({
+                discordId: message.author,
+                displayName: message.member.displayName,
+                server: server,
+            });
 
-		message.channel.send(`Added ${message.member.displayName} to Players model!`);
+            Players.create(newPlayer);
+
+            message.channel.send(`Added ${message.member.displayName} to Players model!`);
+
+            const testRole = message.guild.roles.cache.find(role => role.name === 'Test');
+            message.member.roles.add(testRole);
+        } else {
+            message.channel.send('Invalid `server` argument!');
+        }
 	},
 };
--- models/players.js
+++ models/players.js
@@ -5,6 +5,20 @@ const Schema = mongoose.Schema;
 const players = new Schema({
   discordId: String,
   displayName: String,
+  mmr: {
+    type: Number,
+    default: 1000,
+  },
+  rank: {
+    type: String,
+    enum: ['Bronze', 'Silver', 'Gold', 'Platinum', 'Diamond', 'Champion', 'Grand Champion'],
+    default: 'Platinum',
+  },
+  server: {
+    type: String,
+    enum: ['US-WEST', 'US-EAST', 'EU'],
+    default: 'US-EAST',
+  },
 });
 
 const model = mongoose.model('players', players);
